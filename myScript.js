f3ed7bc8d3a8794b1518fbd677b61f2930379d78
redbrokenapple <blackink86@gmail.com>
2022-06-03 20:38:20 +0900

    rrules for rock,paper,scissors

--- myScript.js
+++ myScript.js
@@ -8,3 +8,35 @@ function computerPlay() {
        console.log ("scissors");
     }
 }
+console.log(computerPlay())
+
+function rules (playerSelection, computerSelection) {
+//rock
+    if(playerSelection == "rock" && computerSelection == "scissors"){
+         return "Good job! You had a one in three chance to win and you did.  Congratulations...";
+    } else if (playerSelection == "rock" && computerSelection == "rock") {
+         return "Oh, Well. It could be worse. You Could of Chosen paper."
+    }  else if  (playerSelection == "rock" && computerSelection == "paper") {
+         return "AHAHAHA. You lost. Loser"
+    }
+
+
+//paper
+    if(playerSelection == "paper" && computerSelection == "rock"){
+     return "Good job! You had a one in three chance to win and you did.  Congratulations...";
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+     return "Oh, Well. It could be worse. You Could of Chosen paper."
+    }  else if (playerSelection == "paper" && computerSelection == "scissors") {
+     return "AHAHAHA. You lost. Loser"
+    }
+
+
+//scissors
+  if(playerSelection == "scissors" && computerSelection == "paper") {
+     return "Good job! You had a one in three chance to win and you did.  Congratulations...";
+   } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+     return "Oh, Well. It could be worse. You Could of Chosen paper."
+   }  else if  (playerSelection == "scissors" && computerSelection == "rock") {
+     return "AHAHAHA. You lost. Loser"
+   }
+}
