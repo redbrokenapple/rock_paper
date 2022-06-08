85ebf2a40030ebc528c1d601dc101ef4749742bd
redbrokenapple <blackink86@gmail.com>
2022-06-08 18:37:04 +0900

    create loop; add user prompt for cancel

--- myScript.js
+++ myScript.js
@@ -1,11 +1,10 @@
-
-const computerSelection = computerPlayer();
-const playerSelection = humanPlayer();
-
-function playRound(playerSelection, computerSelection)  {
+function playRound()  {
+     
+  const computerSelection = computerPlayer();
+  const playerSelection = humanPlayer();  
      //plays one round
      //rock
-if(playerSelection === "rock" && computerSelection === "scissors"){
+   if(playerSelection === "rock" && computerSelection === "scissors"){
      console.log( "Good job! You had a one in three chance to win and you did. Congratulations...");
      } else if (playerSelection === "rock" && computerSelection === "rock") {
      console.log ("Oh, Well. It could be worse. You Could of Chosen paper.");
@@ -18,7 +17,7 @@ if(playerSelection === "rock" && computerSelection === "scissors"){
      } else if (playerSelection === "paper" && computerSelection === "paper") {
      console.log( "Oh, Well. It could be worse. You Could of Chosen scissors.");     
      } else if (playerSelection === "paper" && computerSelection === "scissors") {
-     return "AHAHAHA. You lost. Loser."
+     console.log ("AHAHAHA. You lost. Loser.");
      }
      //scissors
      else if(playerSelection === "scissors" && computerSelection === "paper") {
@@ -28,7 +27,7 @@ if(playerSelection === "rock" && computerSelection === "scissors"){
      } else if (playerSelection === "scissors" && computerSelection === "rock") {
      console.log( "AHAHAHA. You lost. Loser.");
      } else {
-     console.log( "Good-bye");
+     console.log("You can't choose that!");
      }
 }
 
@@ -36,22 +35,25 @@ if(playerSelection === "rock" && computerSelection === "scissors"){
 function humanPlayer() {
 //sets rules for player
 //prompts player
-     let input = prompt("what do you choose, rock, paper, or scissors?").toLocaleLowerCase();
+     let input = prompt("what do you choose, rock, paper, or scissors?");
+     if (input === null) {
      return input;
      }
+}
 
 
 function computerPlayer() {
-let choices = ["rock", "paper", "scissors"]     
-return choices[Math.floor(Math.random()*choices.length)]
+     let choices = ["rock", "paper", "scissors"]     
+     return choices[Math.floor(Math.random()*choices.length)]
 }
 
 let playerPoints = 0;
 let computerPoints = 0;  
 
 function game()  {
-     for (let i = 0; i < 5; i++) {
-        playRound(playerSelection, computerSelection);
-          }
+     for (let i = 0; i <= 5; i++) {
+        playRound();
      }
-console.log(game());
+}
+
+console.log(game())
